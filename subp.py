57b8eb7a1f6ba5d9082c227e9e095d473ab7df30
Devin Cao <devincao@csu.fullerton.edu>
2018-07-23 13:13:52 -0700

    Update subp.py

--- subp.py
+++ subp.py
@@ -1,5 +1,6 @@
 #solution for a user calling 'ls'
 #calls ls -l in current directory
+#will need to print this to the user
 #must use python 3.7 for capture_output parameter
 #other possible solution is os.scandir()
 #https://docs.python.org/3/library/os.html#os.scandir
